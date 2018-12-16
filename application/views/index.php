a361d316084d9098f17172ab1897e91adee22c99
Tim Van den Enden <99038115@mydavinci.nl>
2018-12-16 08:21:55 +0100

    Added that it uses random background.

--- application/views/index.php
+++ application/views/index.php
@@ -118,6 +118,23 @@
 		</div>
 		
 		<script>
+			
+			$('.bg').css("background-image", "url(../assets/images/bg/bg" + randomIntFromInterval(1, 5) + ".png)");  
+			
+
+		    function randomIntFromInterval(min,max)
+		    {
+		        let result =  Math.floor(Math.random()*(max-min+1)+min);
+		        if(result < min){
+		            result = min;
+		        }
+		        else if(result > max){
+		            result = max;
+		        }
+		        return result;
+		    }
+
+
 			var words = [{word:"demo"}, {word:"test"}];
 			var pos = 0;
 			var tryCount = 0;
